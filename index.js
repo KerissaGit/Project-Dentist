bb1e1c23e28bd6338406507f6c52fff062842baf
KerissaGit <kerissa525@gmail.com>
2025-02-07 11:58:26 -0500

    Updated HTML and prep for js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+//Fetch Data for Patient infor
 const displayPatients = () => {
     fetch('http://localhost:3000/patients')
     .then(response=>response.json())
@@ -13,3 +13,16 @@ const displayPatients = () => {
       })
     })
   };
+
+  //Globals 
+
+
+  //Access and display list of patients
+  //Click event to display current patient
+  //Add new patients
+  //Create button to append new patients to current patient list
+  //Submit form for new patient
+  //Display patient profile in center of page
+  //Create button for pulling profile to add to call list
+  //Create a form to make an appointment
+  
