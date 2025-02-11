ef62f33f61e8f53476da3f1269bc84e1d6ca8f7a
KerissaGit <kerissa525@gmail.com>
2025-02-11 13:22:30 -0500

    Updated CSS to look more appealing. Also in JS started writing code to potentially be used for click events and submit events. Not functional at this time.

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const callList = document.getElementById("#call-list")
 
 
 //Fetch Data for Patient information
+//Displays list of our patients
 const displayPatients = () => {
     fetch(BASE_URL)
     .then(response=>response.json())
@@ -27,6 +28,7 @@ const displayPatients = () => {
         img.alt = `${patient.name}`
 
         const patientName = document.createElement('h3')
+        patientTab.addEventListener('click', () => patientClick(patient))
 
         patientList.append(patientTab, patientName)
   
@@ -36,12 +38,6 @@ const displayPatients = () => {
 
   displayPatients();
 
-  function getPatientsData() {
-    fetch(BASE_URL)
-    .then((resp) => resp.json())
-    .then((patients) => patients.forEach(displayPatients))
-    
-  }
 
 
 
@@ -56,18 +52,68 @@ const displayPatients = () => {
   //Create button for pulling profile to add to call list
   //Create a form to make an appointment
   
+// -------EXPERIMENTAL CODE---------
+//Code was being built for click functions of patients
+// const patientClick = (pats) => {
+//   patientName.textContent = pats.name
+//   patientPhone.textContent = pats.phone
+//   patientBirthday.textContent = pats.birthday
+//   patientsLastVisit.textContent = pats.last_visited
+//   patientMessage.textContent = pats.message
+//   patientXray.src = image.image
+// };
+
+// const addSubmitListener = () => {
+//   const form = document.getElementById('patient-info')
+//   form.addEventListener('submit', (event) => {
+//     event.preventDefault()
+
+ 
+//     const name = document.getElementById('name').value
+//     const phone = document.getElementById('phone').value
+//     const birthday = document.getElementById('birthday').value
+//     const lastVisit = document.getElementById('last-visit').value
+//     const message = document.getElementById('message').value
+//     const image = document.getElementById('image').value
+
+//     const newPatient = {name, phone, birthday, lastVisit, message, image}
+    
+//     const xrayImage = document.createElement('img')
+//     xrayImage.src = image.image
+//     xrayImage.alt = image.name
+//     xrayImage.addEventListener('click', () => patientClick(newPatient))
+//     currentPatient.append(xrayImage)
+
+//     form.reset()
+//   }) 
+// };
+
+// addSubmitListener()
 
 
+//Booking appointments to update appointment date
+// const bookAppointment = document.querySelector('#patient-appointment');
+// bookAppointment.addEventListener('submit', (e) => {
+//   e.preventDefault();
+//   const nextVisit = document.getElementById('next-visit')
+//   const patientId = currentPatient.dataset.id
+// },
+// //Trying to make a POST method to update to a new appointment date.
+// fetch(`${BASE_URL}`, `${patient.id}`),
+//   method: 'POST',
+//   headers: {
+//     'Content-Type': 'application/json',
+//     Accept: 'application/json',
+//   },
+//   body: JSON.stringify(nextVisit)
 
-
-
+// )
 
 
 
   
   //Yeji code for adding new patients
-  const form = document.querySelector(".patient-form")
-  console.log(form)
+  const form = document.querySelector("patient-form")
   form.addEventListener('submit',(event)=>{
     event.preventDefault()
     const newPatient={
