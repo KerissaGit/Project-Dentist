7160973a09707462ca947d456f3e4bbc1f02bcf8
KerissaGit <kerissa525@gmail.com>
2025-02-12 13:07:59 -0500

    Adjusting phone number look and input. Added text to current patient profile. Debuging lack of image url input to new patient.

--- index.js
+++ index.js
@@ -25,39 +25,32 @@ const openProfile = (patient) =>{
   const name = document.querySelector("#patient-name")
   name.textContent = patient.name
   const phone = document.querySelector("#patient-phone")
-  phone.textContent = patient.phone_number
+  phone.textContent = 'Phone Number: ' + patient.phone_number
   const birthday = document.querySelector("#patient-birthday")
-  birthday.textContent=patient.birthday
+  birthday.textContent= 'Birthday: ' + patient.birthday
   const lastvisit = document.querySelector("#patient-last-visit")
-  lastvisit.textContent=patient.last_visited
+  lastvisit.textContent= 'Last Date Visited: ' + patient.last_visited
   const message = document.querySelector("#patient-message")
-  message.textContent=patient.message
+  message.textContent= 'Special Note: ' + patient.message
   const form = document.querySelector(".patient-appointment")
 
-  form.reset()
-
-  form.removeEventListener("submit", handleSubmit)
-
-  form.addEventListener("submit", handleSubmit)
-
-  form.reset()
-
-
-    
+  
+  
+  
   function handleSubmit(e){
     e.preventDefault()
    const newappointment = e.target["next-visit"].value
+   
   
-  
-  
-  fetch(`http://localhost:3000/patients/${patientcurrent.id}`,{
-    method: `PATCH`,
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify ({
-      appointment:newappointment
-    })})
+   
+   fetch(`http://localhost:3000/patients/${patientcurrent.id}`,{
+     method: `PATCH`,
+     headers: {
+       "Content-Type": "application/json"
+      },
+      body: JSON.stringify ({
+        appointment:newappointment
+      })})
 
     .then(resp=>resp.json())
     .then((data)=>{
@@ -65,79 +58,86 @@ const openProfile = (patient) =>{
       document.querySelector("#patient-appointment").textContent = newappointment
     })
     .catch((error) => console.error("Error updating appointment:", error));
-
-
-      
-  
-
+    
+    
+    
+    
+    
   }
 }
 
 
 //Fetch Data for Patient information
 const displayPatients = () => {
-    fetch(BASE_URL)
-    .then(response=>response.json())
-    .then(patients=>{
-      //const data = patients.data
+  fetch(BASE_URL)
+  .then(response=>response.json())
+  .then(patients=>{
+    //const data = patients.data
       const patientList = document.querySelector('#patients ul')
-
+      
       patients.forEach(patient => {
-
+        
         const patientTab = document.createElement('li')
         patientTab.textContent = `${patient.name}`
         //patientTab.dataset.patientId = patient.id
-   
+        
         //const img = document.createElement('img')
         //img.src = patient.image
         //img.alt = `${patient.name}`
-
+        
         //const patientName = document.createElement('h3')
         
         patientTab.addEventListener("click",()=>openProfile(patient))
         patientList.append(patientTab)
-   
+        
       })
     })
   };
   
-displayPatients();
-
+  displayPatients();
+  
   
   //Code for adding new patients
   function addnewpatient(){
-  const form = document.querySelector(".patient-form")
-  const patientList1 = document.querySelector('#patients ul')
-  console.log(form)
-  form.addEventListener('submit',(event)=>{
-    event.preventDefault()
-    const newPatient={
-      name:event.target['name'].value,
-      phone_number:event.target['phone'].value,
-      birthday:event.target['birthday'].value,
-      last_visited:event.target['last-visit'].value,
-      message:event.target['message'].value,
-      appointment:"",
-      image_url:""
-    }
-
-    fetch('http://localhost:3000/patients', {
-      method:'POST',
-      headers:{
-         "Content-Type":'application/json'
-       },
-       body: JSON.stringify(newPatient)
-       })
-     .then(resp=>resp.json())
-     .then((data)=>{
-      console.log(data)
-      const newpatientitem = document.createElement('li')
-      newpatientitem.textContent = `${data.name}`
-      patientList1.append(newpatientitem)
-    
+    const form = document.querySelector(".patient-form")
+    const patientList1 = document.querySelector('#patients ul')
+    console.log(form)
+    form.addEventListener('submit',(event)=>{
+      event.preventDefault()
+      const newPatient={
+        name:event.target['name'].value,
+        phone_number:event.target['phone'].value,
+        birthday:event.target['birthday'].value,
+        last_visited:event.target['last-visit'].value,
+        message:event.target['message'].value,
+        appointment:"",
+        image_url: "",
+      }
+      
+      fetch('http://localhost:3000/patients', {
+        method:'PATCH',
+        headers:{
+          "Content-Type":'application/json'
+        },
+        body: JSON.stringify(newPatient)
+      })
+      .then(resp=>resp.json())
+      .then((data)=>{
+        console.log(data)
+        const newpatientitem = document.createElement('li')
+        newpatientitem.textContent = `${data.name}`
+        patientList1.append(newpatientitem)
+        
+      })
     })
-  })
-}
-     
-addnewpatient()
+  }
+  
+  addnewpatient();
    
+  form.reset()
+  
+  form.removeEventListener("submit", handleSubmit)
+  
+  form.addEventListener("submit", handleSubmit)
+  
+  form.reset()
